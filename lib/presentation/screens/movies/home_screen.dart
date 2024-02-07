26952847e236b862b51e472e710a19a7f09a9a21
Rdani2005 <daniel.sequeira@sim.co.cr>
2024-02-07 13:49:42 -0600

    Movies slideshow

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_app/presentation/providers/movies/movies_providers.dart';
+import 'package:movie_app/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-  static const name = 'home-screen';
+  static String name = "HomeScreen";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return Column(
+      children: [
+        const CustomAppBar(),
+        MoviesSlideShow(movies: nowPlayingMovies),
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'package:movie_app/presentation/widgets/movies/movies_slideshow.dart';
+export 'package:movie_app/presentation/widgets/shared/custom_appbar.dart';
